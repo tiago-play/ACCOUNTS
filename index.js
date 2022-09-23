bdd938aa10f1d03657450ee7e61dfdf233a56ede
tiago-play <tiagobr15@gmail.com>
2022-09-22 22:08:02 -0300

    Criação de menu e inicio da criação de contas

--- index.js
+++ index.js
@@ -6,4 +6,33 @@ const chalk = require('chalk')
 //modulos internos
 const fs = require('fs')
 
-console.log("Iniciamos o Accounts")
+//incando a função para que ela seja executada com o sistema
+operation()
+
+function operation() {
+
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: "O que você deseja fazer?",
+            choices: ['Criar conta', 'Consultar Saldo', 'Depositar', 'Sacar', 'Sair'],
+        },
+    ])
+    .then((answer) => {
+
+        const action = answer['action']
+
+        if(action == 'Criar conta'){
+            createAccount()
+        }
+    })
+    .catch((err) => console.log(err))
+}
+
+// create account
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso bano!'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir'))
+    
+}
